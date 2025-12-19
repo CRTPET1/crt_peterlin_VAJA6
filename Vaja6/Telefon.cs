53e78b3fb6f224e78f68d68a99c823a3f8787844
CRTPET1 <crt.peterlin@outlook.com>
2025-12-19 10:00:18 +0100

    Add files via upload

---
+++ Vaja6/Telefon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaja6
+{
+    public class Telefon
+    {
+        private string model;
+        private int letnica;
+
+        public Telefon(string model, int letnica)
+        {
+            this.model = model;
+            this.letnica = letnica;
+        }
+    }
+}
